f6d4237c992cdb60a860916d028fcd1ed9091876
froge <87875402+froge159@users.noreply.github.com>
2022-12-02 22:51:07 -0600

    Updated some variable names

--- password.py
+++ password.py
@@ -3,14 +3,14 @@ lower_letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'
 upper_letters = [i.upper() for i in lower_letters]
 symbols = ["'", "#","$","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~","]"]
 numbers = ['1','2','3','4','5','6','7','8','9','0']
-allchars = lower_letters.copy() + upper_letters.copy() + symbols.copy() + numbers.copy()
+all_chars = lower_letters.copy() + upper_letters.copy() + symbols.copy() + numbers.copy()
 
 def password():
     num_of_chars = int(input('Number of characters: '))
-    pswd = ''
+    password = ''
     for i in range(num_of_chars):
-        pswd += random.choice(allchars)
-    print(pswd)
+        password += random.choice(all_chars)
+    print(password)
     print('')
 
 def newpswd():
@@ -25,7 +25,3 @@ def newpswd():
 
 password()
 newpswd()
-
-
-
-
