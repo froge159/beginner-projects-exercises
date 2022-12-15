5ce92c9d12e43c09fa16aeddfefd6161f777fd52
froge <87875402+froge159@users.noreply.github.com>
2022-12-14 23:00:53 -0600

    reformated list, minor changes

--- alphabetposition.py
+++ alphabetposition.py
@@ -1,19 +1,17 @@
 def alphabet_pos(text):
     ans = []
     alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
-                    'u', 'v', 'w', 'x', 'y', 'z']
+                'u', 'v', 'w', 'x', 'y', 'z']
     for i in text.lower():
-        if i.isalpha() == True:
+        if i.isalpha():
             for k in alphabet:
                 if i == k:
-                     ans.append(alphabet.index(k) + 1)
+                    ans.append(alphabet.index(k) + 1)
         else:
             pass
     res = ' '.join(str(x) for x in ans)
     return res
 
-input = input('')
-print(alphabet_pos(input))
 
-
-
+input_ = input('')
+print(alphabet_pos(input_))
